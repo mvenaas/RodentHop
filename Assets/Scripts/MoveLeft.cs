b147edbe177e76650e7a96b4ba62625890ba6410
Martin Venaas <m@martinvenaas.com>
2017-07-21 18:19:30 +1200

    Added infi-scroll and memory handling

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour {
+
+    public float speed = 10;
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Takes current position of object and moves left "speed" units per deltatime. 
+        // Deltatime is how many seconds last frame used to render. ideally 1 frame/sec?
+        transform.position += Vector3.left * speed * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/PreFabSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreFabSpawner : MonoBehaviour {
+
+    private float nextSpawn = 0;
+    public Transform preFabToSpawn;
+    public float spawnRate = 1;
+    public float randomDelay = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextSpawn)
+        {
+            // Spawns a new cactus 
+            var theClone = Instantiate(preFabToSpawn, transform.position, Quaternion.identity);
+            nextSpawn = Time.time + spawnRate + Random.Range(0, randomDelay);
+            // Cast the clone to transform to be sure you're destroying the instansiated object and not the prefab itself
+            Destroy((theClone as Transform).gameObject, 5);
+        }
+	}
+}
