1ba693caf9f2e83f7743c7610b6074024db7ff78
Martin Venaas <m@martinvenaas.com>
2017-07-24 16:13:53 +1200

    Added scrolling enemies and hurt animation

--- Assets/Scripts/BunnyController.cs
+++ Assets/Scripts/BunnyController.cs
@@ -7,25 +7,44 @@ public class BunnyController : MonoBehaviour {
 
     private Rigidbody2D myRigidBody2d;
     private Animator myAnimator;
-    public float bunnyJumpForce = 500F; 
+    public float bunnyJumpForce = 500F;
+    private float bunnyHurtTime = -1;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         myRigidBody2d = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetButtonUp("Jump")) {
-            myRigidBody2d.AddForce(transform.up * bunnyJumpForce);
+
+        if (bunnyHurtTime == -1)
+        {
+            if (Input.GetButtonUp("Jump"))
+            {
+                myRigidBody2d.AddForce(transform.up * bunnyJumpForce);
+            }
+            myAnimator.SetFloat("vVelocity", myRigidBody2d.velocity.y);
+        } else
+        {
+            if(Time.time > bunnyHurtTime + 2 )
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
         }
-        myAnimator.SetFloat("vVelocity", myRigidBody2d.velocity.y);
 	}
 
     void OnCollisionEnter2D (Collision2D Collision) {
         if (Collision.collider.gameObject.layer == LayerMask.NameToLayer("Obstacles")) {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+            foreach(MoveLeft x in FindObjectsOfType<MoveLeft>())
+            {
+
+            }
+
+            bunnyHurtTime = Time.time;
+            myAnimator.SetBool("bunnyHUrt", true);
         }
     }
 }
