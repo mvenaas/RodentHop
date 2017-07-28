d25a8d7190dcd4e103829d5f9f17134519fea161
Martin Venaas <m@martinvenaas.com>
2017-07-28 18:23:09 +1200

    Added new art, jump restricitions and new memory handeling

--- Assets/Scripts/BunnyController.cs
+++ Assets/Scripts/BunnyController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI; 
 
 public class BunnyController : MonoBehaviour {
 
@@ -9,11 +10,17 @@ public class BunnyController : MonoBehaviour {
     private Animator myAnimator;
     public float bunnyJumpForce = 500F;
     private float bunnyHurtTime = -1;
+    private Collider2D myCollider;
+    public Text scoreText;
+    private float startTime;
+    private int jumpsLeft = 2;
 
     // Use this for initialization
     void Start () {
         myRigidBody2d = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCollider = GetComponent<Collider2D>();
+        startTime = Time.time;
     }
 	
 	// Update is called once per frame
@@ -21,14 +28,22 @@ public class BunnyController : MonoBehaviour {
 
         if (bunnyHurtTime == -1)
         {
-            if (Input.GetButtonUp("Jump"))
-            {
+            if (Input.GetButtonUp("Jump") && jumpsLeft > 0)
+            {                
+
+                if(myRigidBody2d.velocity.y > 0)
+                {
+                    myRigidBody2d.velocity = Vector2.zero;
+                }
+
                 myRigidBody2d.AddForce(transform.up * bunnyJumpForce);
+                jumpsLeft--;
             }
             myAnimator.SetFloat("vVelocity", myRigidBody2d.velocity.y);
+            scoreText.text = (Time.time - startTime).ToString("0.0");
         } else
         {
-            if(Time.time > bunnyHurtTime + 2 )
+            if(Time.time > bunnyHurtTime + 4 )
             {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
@@ -40,11 +55,24 @@ public class BunnyController : MonoBehaviour {
 
             foreach(MoveLeft x in FindObjectsOfType<MoveLeft>())
             {
+                x.enabled = false;
+            }
 
+            foreach (PreFabSpawner spawner in FindObjectsOfType<PreFabSpawner>())
+            {
+                spawner.enabled = false;
             }
 
             bunnyHurtTime = Time.time;
-            myAnimator.SetBool("bunnyHUrt", true);
+            myAnimator.SetBool("bunnyHurt", true);
+            myRigidBody2d.velocity = Vector2.zero;
+            myRigidBody2d.AddForce(transform.up * bunnyJumpForce);
+            myCollider.enabled = false;
+        }
+
+        if (Collision.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            jumpsLeft = 2; 
         }
     }
 }
--- Assets/Scripts/PreFabSpawner.cs
+++ Assets/Scripts/PreFabSpawner.cs
@@ -6,12 +6,15 @@ public class PreFabSpawner : MonoBehaviour {
 
     private float nextSpawn = 0;
     public Transform preFabToSpawn;
-    public float spawnRate = 1;
-    public float randomDelay = 1;
+    public AnimationCurve spawnCurve;
+    public float curveLengthInSeconds = 30;
+    private float startTime;
+    public float jitter = 0.25f;
 
 	// Use this for initialization
 	void Start () {
-		
+        startTime = Time.time;
+
 	}
 	
 	// Update is called once per frame
@@ -20,9 +23,21 @@ public class PreFabSpawner : MonoBehaviour {
         {
             // Spawns a new cactus 
             var theClone = Instantiate(preFabToSpawn, transform.position, Quaternion.identity);
-            nextSpawn = Time.time + spawnRate + Random.Range(0, randomDelay);
+            // nextSpawn = Time.time + spawnRate + Random.Range(0, randomDelay);
             // Cast the clone to transform to be sure you're destroying the instansiated object and not the prefab itself
-            Destroy((theClone as Transform).gameObject, 5);
+            //Destroy((theClone as Transform).gameObject, 5);
+
+            float curvePos = (Time.time - startTime) / curveLengthInSeconds; 
+
+            if (curvePos < 1F)
+            {
+                curvePos = 1F;
+                startTime = Time.time;
+            }
+
+            nextSpawn = Time.time + spawnCurve.Evaluate(curvePos) + Random.Range(-jitter, jitter);
+
         }
 	}
+
 }
